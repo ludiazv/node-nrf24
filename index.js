e112047cd9a6e317c1de1233aa9d32e53fa8b1fb
L Diaz <ldiaz@navarone-3.local>
2018-05-21 20:56:07 +0200

    mod index.js (error msg & undefined vs null)

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-var noderf24=null;
+var noderf24=undefined;
 
 if(process.platform != "linux") {
-  console.log("Error loading node-rf24. This is LINUX only node addon");
+  console.error("Error loading nRF24. This is LINUX only node addon");
 } else {
   //noderf24= require("./build/Release/nRF24");
   noderf24=require('bindings')('nRF24.node');
