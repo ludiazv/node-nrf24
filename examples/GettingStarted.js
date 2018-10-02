90da003d5d3d468098a5b46fbf9cb96dc06512b4
L Diaz <ldiaz@navarone-3.local>
2018-10-02 18:12:08 +0200

    aling & improve gettingstarte example

--- examples/GettingStarted.js
+++ examples/GettingStarted.js
@@ -18,17 +18,17 @@ if(!ready) {
   process.exit(1);
 }
 
-var config={PALevel:rf.RF24_PA_LOW,
-            DataRate:rf.RF24_250KBPS,
+var config={PALevel:rf.RF24_PA_MAX,
+            DataRate:rf.RF24_1MBPS,
             Channel:76,
-            AutoAck:false,
+            AutoAck:true,
             Irq: IRQ
           };
 
 
 radio.config(config,true);
 
-var Pipes= ["0x65646f4e31","0x65646f4e32"];
+var Pipes= ["0x65646f4e31","0x65646f4e32"]; // "1Node", "2Node"
 
 function rcv(){
 
@@ -43,11 +43,11 @@ function rcv(){
 
 }
 
-function snd() {
+function snd(do_sync) {
 
   radio.useWritePipe(Pipes[1]);
   radio.addReadPipe(Pipes[0]);
-  console.log("Open Write Pipe "+ Pipes[0]);
+  console.log("Open Write Pipe "+ Pipes[1]+ " reading from " + Pipes[0]);
 
   var tmstp;
   var roundtrip;
@@ -57,10 +57,15 @@ function snd() {
     data.writeUInt32LE(tmstp);
     process.stdout.write("\nSending....");
     roundtrip=process.hrtime();
-    radio.write(data,function(success){
-      if(success) process.stdout.write("Sended " + tmstp);
-      else process.stdout.write("Failed to send " + tmstp +" ");
-    });
+    if(do_sync) {
+       if(radio.write(data)) process.stdout.write("[Sync] Sended " + tmstp);
+       else process.stdout.write("[Sync] Failed to send " + tmstp +" ");
+    } else {
+      radio.write(data,function(success){
+        if(success) process.stdout.write("[Async] Sended " + tmstp);
+        else process.stdout.write("[Async] Failed to send " + tmstp +" ");
+      });
+    }
   };
   radio.read(function(d,n) {
     if(n==1){
@@ -87,7 +92,17 @@ console.log("This script assumes that this radio is radionumber=1 in ardunino.")
 console.log("Other radio must be configured with radionumber=0")
 rl.question("[s]ender or [R]eceiver?",(resp)=>{
   var mode=resp.charAt(0);
-  console.log("Running... press CTRL+C to stop test");
-  if(mode.toUpperCase()=="S") snd(); else rcv();
-  rl.close();
+  if(mode.toUpperCase()=="S") {
+    rl.question("Send [s]ync or [A]sync?",(resp2)=>{
+       var asy=resp2.charAt(0);
+       console.log("Starting snd... press CTRL+C to stop");
+       if(asy.toUpperCase()=="S") snd(true);
+       else snd(false);
+       rl.close(); 
+    }); 
+  }else {
+    console.log("Starting rcv.... press CTRL+C to stop");
+    rcv();
+    rl.close();
+  }
 });
