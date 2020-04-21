f284b2dac6736b89d17c2b91c1686cfb5db30229
L Diaz <ldiaz@navarone.local>
2020-04-21 16:37:40 +0200

    travis minimal

--- docker_build.sh
+++ docker_build.sh
@@ -9,7 +9,8 @@ echo "Building with docker for node $DOCKER_NODE"
 docker run -t --rm -v $(pwd):/root/node-nrf24 \
            --workdir /root/node-nrf24 \
            $DOCKER_NODE \
-           /bin/bash -c "npm install node-gyp -g && ./build_rf24libs.sh && node-gyp rebuild"
-
+#           /bin/bash -c "npm install node-gyp -g && ./build_rf24libs.sh && node-gyp rebuild"
+            /bin/bash -c "ls"
+            
 echo "Finished with: $?"
 echo "done!" 
