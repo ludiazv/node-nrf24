9cbb7ebf00062af0aafc3a93d6408912ae045b8a
L Diaz <ldiaz@navarone.local>
2020-04-21 23:32:34 +0200

    travis build fixed

--- docker_build.sh
+++ docker_build.sh
@@ -6,12 +6,14 @@ DOCKER_NODE="arm32v7/node:10-buster"
 [ -n "$1" ] && DOCKER_NODE="$1"
 
 echo "Building with docker for node $DOCKER_NODE"
-#docker run --rm --privileged multiarch/qemu-user-static:register
-docker run -t --rm -v $(pwd):/root/node-nrf24 \
-           --workdir /root/node-nrf24 \
-           $DOCKER_NODE \
+#docker run --rm --privileged multiarch/qemu-user-static:register --reset -p yes
+#docker run -t --rm -v $(pwd):/root/node-nrf24 \
+#           --workdir /root/node-nrf24 \
+#           $DOCKER_NODE \
 #           /bin/bash -c "npm install node-gyp -g && ./build_rf24libs.sh && node-gyp rebuild"
-            /bin/bash -c "node --version"
-            
+#            /bin/bash -c "node --version"
+docker run -t --rm --entrypoint="/bin/sh" -v$(pwd):/root/app --workdir /root/app $NODE_VERSION \
+       -c "npm --version && npm install node-gyp -g && ./build_rf24libs.sh && node-gyp rebuild"
+
 echo "Finished with: $?"
 echo "done!" 
