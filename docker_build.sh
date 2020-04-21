c1fa4e409cabe8b0132fe1f5482354f24f14641b
L Diaz <ldiaz@navarone.local>
2020-04-21 16:43:53 +0200

    fix travis tst

--- docker_build.sh
+++ docker_build.sh
@@ -6,6 +6,7 @@ DOCKER_NODE="arm32v7/node:10-buster"
 [ -n "$1" ] && DOCKER_NODE="$1"
 
 echo "Building with docker for node $DOCKER_NODE"
+docker run --rm --privileged multiarch/qemu-user-static:register
 docker run -t --rm -v $(pwd):/root/node-nrf24 \
            --workdir /root/node-nrf24 \
            $DOCKER_NODE \
