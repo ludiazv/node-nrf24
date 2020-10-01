9a9cee55fbf81dd087efebf2e45863f9a3bba4ab
L Diaz <ldiaz@navarone.local>
2020-10-01 17:36:48 +0200

    migration to node-gyp-build with prebuildify

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ if(process.platform != "linux") {
   console.error("Error loading nRF24. This is LINUX only node addon");
 } else {
   //noderf24= require("./build/Release/nRF24");
-  noderf24=require('bindings')('nRF24.node');
+  //noderf24=require('bindings')('nRF24.node');
+  noderf24 = require('node-gyp-build')(__dirname);
 }
 
 module.exports= noderf24;
